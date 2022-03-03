259a93868aedfb6e845201f3b9445d30c5fc06ac
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2022-03-03 19:42:55 +0700

    Updated Execution Time

--- Execution Time (Chrono).cpp
+++ Execution Time (Chrono).cpp
@@ -11,12 +11,15 @@ using namespace std;
 
 void constantFunction(int input)        //Decide Odd-Even
 {
+    for(int i = 0; i < 10000000; i++)   //Looped 10 million times to get observable results
+    {
     string parity;
     int total = input%2;
     if(total == 0)
         parity = "even";
         else
             parity = "odd";
+    }
 }
 
 void linearFunction(int input)          //Going through a loop
