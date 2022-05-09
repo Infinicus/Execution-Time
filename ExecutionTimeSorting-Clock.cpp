7254b72f3faad11460c415c481056aef22d42c25
Giga Hidjrika Aura Adkhy <gigahidjrikaaa@gmail.com>
2022-05-09 17:12:03 +0700

    Renamed and added sorting
    I used Codeblocks back then, hence the code name with the spaces :D

---
+++ ExecutionTimeSorting-Clock.cpp
@@ -0,0 +1,216 @@
+//Code to check execution time using clock
+//May not show values more than 3 decimals after zero, for higher precision, use chrono
+
+#include <iostream>
+#include <time.h>
+#include <iomanip>
+#include <string>
+#include <random>
+#include <algorithm>
+#define PRECISION 5                     //How many decimals
+#define SIZE 10000                   //Size of array
+
+using namespace std;
+
+int is_sorted(int *arr, int n) {
+   while ( --n >= 1 ) {
+      if ( arr[n] < arr[n-1] ) {
+         return 0;
+      }
+   }
+   return 1;
+}
+void shuffle(int *arr, int n) {
+   int temp, r;
+   for(int i=0; i < n; i++) {
+      temp = arr[i];
+      r = rand() % n;
+      arr[i] = arr[r];
+      arr[r] = temp;
+   }
+}
+void bogosort(int *arr, int n) {
+   while ( !is_sorted(arr, n) ) {
+      shuffle(arr, n);
+   }
+}
+
+//execution time of bogosort
+void bogoSortExecutionTime(int input[], int size)
+{
+    clock_t start = clock();
+    bogosort(input, size);
+    clock_t end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    cout << "Bogo Sort Execution Time: " << fixed << setprecision(PRECISION) << time_taken << " seconds" << endl;
+}
+
+//selection sort
+void selectionSort(int input[], int size)
+{
+    int i, j, min_idx, temp;
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (input[j] < input[min_idx])
+            {
+                min_idx = j;
+            }
+        }
+        temp = input[i];
+        input[i] = input[min_idx];
+        input[min_idx] = temp;
+    }
+}
+
+//execution time of selection sort
+void selectionSortExecutionTime(int input[], int size)
+{
+    clock_t start = clock();
+    selectionSort(input, size);
+    clock_t end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    cout << "Selection Sort Execution Time: " << fixed << setprecision(PRECISION) << time_taken << " seconds" << endl;
+}
+
+//insertion sort
+void insertionSort(int input[], int size)
+{
+    int i, j, key;
+    for (i = 1; i < size; i++)
+    {
+        key = input[i];
+        j = i - 1;
+        while (j >= 0 && input[j] > key)
+        {
+            input[j + 1] = input[j];
+            j = j - 1;
+        }
+        input[j + 1] = key;
+    }
+}
+
+//execution time of insertion sort
+void insertionSortExecutionTime(int input[], int size)
+{
+    clock_t start = clock();
+    insertionSort(input, size);
+    clock_t end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    cout << "Insertion Sort Execution Time: " << fixed << setprecision(PRECISION) << time_taken << " seconds" << endl;
+}
+
+//bubble sort
+void bubbleSort(int input[], int size)
+{
+    int i, j, temp;
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (input[j] > input[j + 1])
+            {
+                temp = input[j];
+                input[j] = input[j + 1];
+                input[j + 1] = temp;
+            }
+        }
+    }
+}
+
+//execution time of bubble sort
+void bubbleSortExecutionTime(int input[], int size)
+{
+    clock_t start = clock();
+    bubbleSort(input, size);
+    clock_t end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    cout << "Bubble Sort Execution Time: " << fixed << setprecision(PRECISION) << time_taken << " seconds" << endl;
+}
+
+
+//generate random integer array
+void generateRandomArray(int input[], int size)
+{
+    srand(time(NULL));
+    for(int i = 0; i < size; i++)
+    {
+        input[i] = rand() % 100;
+    }
+}
+
+//generate reverse sorted array
+void generateReverseSortedArray(int input[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        input[i] = size - i;
+    }
+}
+
+//generate sorted array
+void generateSortedArray(int input[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        input[i] = i;
+    }
+}
+
+//prints array
+void printArray(int input[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        cout << input[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int numbers[SIZE];
+
+    // generateRandomArray(numbers, SIZE);
+    // bogoSortExecutionTime(numbers, SIZE);
+
+    //prints size
+    cout << "Size: " << SIZE << endl;
+
+    cout << "\nBest Case: " << endl;
+    generateSortedArray(numbers, SIZE);
+    selectionSortExecutionTime(numbers, SIZE);
+
+    generateSortedArray(numbers, SIZE);
+    insertionSortExecutionTime(numbers, SIZE);
+
+    generateSortedArray(numbers, SIZE);
+    bubbleSortExecutionTime(numbers, SIZE);
+
+    cout << "\nAverage case: " << endl;
+    generateRandomArray(numbers, SIZE);
+    selectionSortExecutionTime(numbers, SIZE);
+
+    generateRandomArray(numbers, SIZE);
+    insertionSortExecutionTime(numbers, SIZE);
+
+    generateRandomArray(numbers, SIZE);
+    bubbleSortExecutionTime(numbers, SIZE);
+
+    cout << "\nWorst case: " << endl;
+    generateReverseSortedArray(numbers, SIZE);
+    selectionSortExecutionTime(numbers, SIZE);
+
+    generateReverseSortedArray(numbers, SIZE);
+    insertionSortExecutionTime(numbers, SIZE);
+
+    generateReverseSortedArray(numbers, SIZE);
+    bubbleSortExecutionTime(numbers, SIZE);
+
+    return 0;
+}
+
+//Giga Hidjrika Aura Adkhy
+//02/03/2022
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Execution-Time-Checker
- A program to demonstrate constant, linear, and quadratic function time.
+A program to demonstrate constant, linear, and quadratic function time.
+
+I have also created a version to demonstrate some of the sorting algorithms (selection, insertion, and bubble sort). I also included a bogosort function in the program, just for fun.
+
+Enjoy!
